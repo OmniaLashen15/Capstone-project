3f2cd39a2f8ffb11379755a790868da228253e7c
unknown <https://github.com/OmniaLashen15/FSND-Deploy-Flask-App-to-Kubernetes-Using-EKS.git>
2020-10-23 02:52:31 +0200

    commit

--- models.py
+++ models.py
@@ -18,11 +18,11 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    #db.create_all()
     
 
 
-#migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 '''
 Movies
